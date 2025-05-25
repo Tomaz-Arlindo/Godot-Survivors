cd9c8d72e90976903df8647b72415ffd66e1885a
Tomaz Arlindo <tomaz.recife@gmail.com>
2025-05-24 21:53:09 -0300

    add spear attack

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,8 +3,28 @@ extends CharacterBody2D
 
 var movement_speed = 40.0
 var hp = 80
+
+#Attacks
+var spear = preload("res://Player/Attack/spear.tscn")
+
+#AttackNodes
+@onready var spearTimer = get_node("%SpearTimer")
+@onready var spearAttackTimer = get_node("%SpearAttackTimer")
+
+#Spear
+var spear_ammo = 0
+var spear_baseammo = 1
+var spear_attackspeed = 1.5
+var spear_level = 1
+
+#Enemy Related
+var enemy_close = []
+
 @onready var sprite = $Sprite2D
 @onready var walkTimer = get_node("%walkTimer")
+
+func _ready():
+	attack()
 
 func _physics_process(_delta):
 	movement()
@@ -29,7 +49,47 @@ func movement():
 	velocity = mov.normalized()*movement_speed
 	move_and_slide()
 
+func attack():
+	if spear_level > 0:
+		spearTimer.wait_time = spear_attackspeed
+		if spearTimer.is_stopped():
+			spearTimer.start()
 
 func _on_hurt_box_hurt(damage):
 	hp -= damage
 	print(hp)
+
+
+func _on_spear_timer_timeout():
+	spear_ammo += spear_baseammo
+	spearAttackTimer.start()
+
+
+func _on_spear_attack_timer_timeout():
+	if spear_ammo > 0:
+		var spear_attack = spear.instantiate()
+		spear_attack.position = position
+		spear_attack.target = get_random_target()
+		spear_attack.level = spear_level
+		add_child(spear_attack)
+		spear_ammo -= 1
+		if spear_ammo > 0:
+			spearAttackTimer.start()
+		else:
+			spearAttackTimer.stop()
+			
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body):
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body):
+	if enemy_close.has(body):
+		enemy_close.erase(body)
